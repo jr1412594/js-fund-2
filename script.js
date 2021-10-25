89ddefae0a5d04f682da4d5e5e321f776b2a0a08
jr1412594 <jr.medina1412@gmail.com>
2021-10-25 11:35:26 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// 'use strict';
+
+// let hasDriversLicense = false;
+// const passTest = true;
+
+// if (passTest) hasDriversLicense = true;
+
+// if(hasDriversLicense) {
+//     console.log("I can drive!")
+// }
+
+// const interface = "Audio"
+// const private = '420'
+
+function logger() {
+    console.log("my name is Jr");
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    // console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+    return juice
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice)
+
+const appleOrangeJuice = fruitProcessor(2, 4)
+console.log(appleOrangeJuice)
