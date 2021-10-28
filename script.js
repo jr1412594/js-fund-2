96c72128a5893c9123e88ac766a81113224b3b77
jr1412594 <jr.medina1412@gmail.com>
2021-10-28 15:47:11 -0600

    code challenge 4

--- script.js
+++ script.js
@@ -283,13 +283,13 @@
 
 // console.log(`${pierogi.firstName} has ${pierogi.friends.length} friends, and his best friend is called ${pierogi.friends[0]}!`)
 
-const pierogi = {
-    firstName: 'Pierogi',
-    lastName: 'Pup',
-    birthYear: 2019,
-    job: 'Guard Dog',
-    friends: ['Miso', 'Mom', 'Dad', 'Curly', 'Dino'],
-    hasDriversLicense: false,
+// const pierogi = {
+//     firstName: 'Pierogi',
+//     lastName: 'Pup',
+//     birthYear: 2019,
+//     job: 'Guard Dog',
+//     friends: ['Miso', 'Mom', 'Dad', 'Curly', 'Dino'],
+//     hasDriversLicense: false,
     
     // calcAge: function(birthYear) {
     //     return 2021 - birthYear
@@ -300,24 +300,72 @@ const pierogi = {
     //     return 2021 - this.birthYear
     // }
 
-    calcAge: function() {
-        this.age = 2021 - this.birthYear;
-        return this.age
+//     calcAge: function() {
+//         this.age = 2021 - this.birthYear;
+//         return this.age
 
-    },
-    canDrive: function() {
-        return this.hasDriversLicense ? "a" : "no" 
-    },
-    getSummary: function() {
-        return `${this.firstName} is a ${this.age}-year old ${this.job}, and he has ${this.canDrive()} drivers License`
+//     },
+//     canDrive: function() {
+//         return this.hasDriversLicense ? "a" : "no" 
+//     },
+//     getSummary: function() {
+//         return `${this.firstName} is a ${this.age}-year old ${this.job}, and he has ${this.canDrive()} drivers License`
+//     }
+
+// }
+
+// console.log(pierogi)
+// console.log(pierogi.calcAge())
+// console.log(pierogi.age,'this is the age from this . age')
+// // pierogi.hasDriversLicense = true;
+
+// console.log(pierogi.getSummary())
+// console.log(`${pierogi.firstName} is a ${pierogi.age}-year old ${pierogi.job}, and he has ${pierogi.canDrive()} drivers License`)
+
+
+//CHALLENGE # 4!!!
+/*
+Let's go back to Mark and John comparing their BMIs! This time, let's use objects to implement the calculations
+remember : BMI = mass / height ** 2 = mass ? (height * height). (mass in kg and height in meters)
+
+1) For each of them, create an object with properties for their full name, mass, and height (Mark Miller and John Smith)//Done
+
+2) create a 'calcBMI' method on each object to caluculate the BMI (the same method on both objects)
+    Store the BMI value to a property, and also return it from the method.
+
+3) log to the consolel who has the higher BMI, together with the full name an dthe respective BMI.
+    Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
+
+Test data: Marks weight 78 kg, and is 1.69 m tall.
+            John weight 92 kg and is 1.95 m tall.
+*/
+
+const mark = {
+    fullName: 'Mark Miller',
+    mass: 78,
+    height: 1.69,
+    calcBMI: function () {
+        this.bmi =  this.mass / this.height ** 2;
+        return this.bmi;
     }
+}
+mark.calcBMI()
+
+const john = {
+    fullName: 'John Smith',
+    mass: 92,
+    height: 1.95,
+    calcBMI: function () {
+        this.bmi = this.mass / this.height ** 2;
+        return this.bmi;
+    }
+}
+john.calcBMI()
+
+
+if(mark.bmi > john.bmi) {
+    console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})!`)
+} else {
+    console.log(`${john.fullName}'s BMI (${$john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})!`)
 
 }
-
-console.log(pierogi)
-console.log(pierogi.calcAge())
-console.log(pierogi.age,'this is the age from this . age')
-// pierogi.hasDriversLicense = true;
-
-console.log(pierogi.getSummary())
-// console.log(`${pierogi.firstName} is a ${pierogi.age}-year old ${pierogi.job}, and he has ${pierogi.canDrive()} drivers License`)
